a62868fba7bff542345830c597969120eca3ef04
lakshmanchilukuri <lakshmanchilukuri11@gmail.com>
2020-12-17 21:14:06 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express=require('express');
+
+const app=express();
+
+const port=5000;
+
+app.get('/',(req,res)=>res.json({"msg":"in end point"}  ))
+
+app.listen(port, ()=>console.log('server started at '+port));
